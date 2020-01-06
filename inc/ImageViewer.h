2970347590a2e098b93959451ee9299d1110a46e
Alex Rickert <ARICKERT0003@gmail.com>
2020-01-06 02:03:54 -0500

    remove increment string from file destination due to wrong location i.e. image.png1

--- inc/ImageViewer.h
+++ inc/ImageViewer.h
@@ -11,67 +11,70 @@
 struct Window;
 class ImageViewer;
 
-struct Window
+namespace ImgProc
 {
-  Window(){}
-  Window(const std::string& aName)
-  { name = aName;}
-
-  void setImage(cv::Mat& aimage)
-  { 
-    std::lock_guard<std::mutex> guard(mu);
-    image = aimage;
-  }
-
-  void setWriteDir(std::string awriteDir)
+  struct Window
   {
-    std::lock_guard<std::mutex> guard(mu);
-    writeDir = awriteDir;
-  }
+    Window(){}
+    Window(const std::string& aName)
+    { name = aName;}
 
-  bool writeImage()
-  { 
-    writeDir += std::to_string(writeCount);
-    writeStatus = cv::imwrite(writeDir, image);
-    writeCount++;
-    return writeStatus; 
-  }
+    void setImage(cv::Mat& aimage)
+    { 
+      std::lock_guard<std::mutex> guard(mu);
+      image = aimage;
+    }
 
-  void addTrackbar(const std::string& trackbarName, int* value, int max)
+    void setWriteDir(const std::string& awriteDir)
+    {
+      std::lock_guard<std::mutex> guard(mu);
+      writeDir = awriteDir;
+    }
+
+    bool writeImage()
+    { 
+      //writeDir += std::to_string(writeCount);
+      writeStatus = cv::imwrite(writeDir, image);
+      writeCount++;
+      return writeStatus; 
+    }
+
+    void addTrackbar(const std::string& trackbarName, int* value, int max)
+    {
+      std::lock_guard<std::mutex> guard(mu);
+      cv::createTrackbar(trackbarName, name, value, max);
+    }
+
+    bool writeStatus = false;
+    int writeCount = 0;
+    std::string name;
+    std::string writeDir;
+    cv::Mat image;
+    std::mutex mu;
+  };
+
+  class ImageViewer
   {
-    std::lock_guard<std::mutex> guard(mu);
-    cv::createTrackbar(trackbarName, name, value, max);
-  }
+  public:
+    void start();
+    void stop();
+    bool addWindow(std::string);
+    void setWriteDir(const std::string&, const std::string&);
+    bool updateWindow(std::string, cv::Mat&);
+    bool getStatus();
+    void addTrackbarRGB(const std::string&);
+    void getTrackbarRGBValues(std::array<int,6>&);
 
-  bool writeStatus = false;
-  int writeCount = 0;
-  std::string name;
-  std::string writeDir;
-  cv::Mat image;
-  std::mutex mu;
-};
-
-class ImageViewer
-{
-public:
-  void start();
-  void stop();
-  bool addWindow(std::string);
-  void setWriteDir(std::string, std::string);
-  bool updateWindow(std::string, cv::Mat&);
-  bool getStatus();
-  void addTrackbarRGB(const std::string&);
-  void getTrackbarRGBValues(std::array<int,6>&);
-
-private:
-  void loop();
-  void saveAllImages();
-  bool _loopStatus = false;
-  std::array<int, 6> _rgbArray = {0,0,0,255,255,255};
-  std::thread _viewerThread;
-  std::map<std::string, std::unique_ptr<Window>> _windowMap;
-  std::map<std::string, std::unique_ptr<Window>>::iterator _iWindow;
-  std::mutex _mu;
-};
+  private:
+    void loop();
+    void saveAllImages();
+    bool _loopStatus = false;
+    std::array<int, 6> _rgbArray = {0,0,0,255,255,255};
+    std::thread _viewerThread;
+    std::map<std::string, std::unique_ptr<Window>> _windowMap;
+    std::map<std::string, std::unique_ptr<Window>>::iterator _iWindow;
+    std::mutex _mu;
+  };
+}
 
 #endif
